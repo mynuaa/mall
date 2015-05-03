1f86be5c7189cdc377a04b261b9d48696a5671bb
Rex Zeng <rex.fgtsky@qq.com>
2015-05-03 17:02:02 +0800

    适配header和footer

--- ThinkPHP/Library/Think/View.class.php
+++ ThinkPHP/Library/Think/View.class.php
@@ -54,6 +54,20 @@ class View {
         return isset($this->tVar[$name])?$this->tVar[$name]:false;
     }
 
+
+    /**
+     * 判断是否是手机
+     * @return boolean
+     */
+    public function is_mobile() {
+        $user_agent = $_SERVER['HTTP_USER_AGENT'];
+        $mobile_agents = Array("240x320","acer","acoon","acs-","abacho","ahong","airness","alcatel","amoi","android","anywhereyougo.com","applewebkit/525","applewebkit/532","asus","audio","au-mic","avantogo","becker","benq","bilbo","bird","blackberry","blazer","bleu","cdm-","compal","coolpad","danger","dbtel","dopod","elaine","eric","etouch","fly ","fly_","fly-","go.web","goodaccess","gradiente","grundig","haier","hedy","hitachi","htc","huawei","hutchison","inno","ipad","ipaq","ipod","jbrowser","kddi","kgt","kwc","lenovo","lg ","lg2","lg3","lg4","lg5","lg7","lg8","lg9","lg-","lge-","lge9","longcos","maemo","mercator","meridian","micromax","midp","mini","mitsu","mmm","mmp","mobi","mot-","moto","nec-","netfront","newgen","nexian","nf-browser","nintendo","nitro","nokia","nook","novarra","obigo","palm","panasonic","pantech","philips","phone","pg-","playstation","pocket","pt-","qc-","qtek","rover","sagem","sama","samu","sanyo","samsung","sch-","scooter","sec-","sendo","sgh-","sharp","siemens","sie-","softbank","sony","spice","sprint","spv","symbian","tablet","talkabout","tcl-","teleca","telit","tianyu","tim-","toshiba","tsm","up.browser","utec","utstar","verykool","virgin","vk-","voda","voxtel","vx","wap","wellco","wig browser","wii","windows ce","wireless","xda","xde","zte");
+        foreach ($mobile_agents as $device)
+            if (stristr($user_agent, $device))
+                return true;
+        return false;
+    }
+
     /**
      * 加载模板和页面输出 可以返回输出内容
      * @access public
@@ -66,6 +80,9 @@ class View {
      */
     public function display($templateFile='',$charset='',$contentType='',$content='',$prefix='') {
         G('viewStartTime');
+        if ($this->is_mobile()) {
+            $templateFile = $templateFile . '_mobile';
+        }
         // 视图开始标签
         Hook::listen('view_begin',$templateFile);
         // 解析并获取模板内容
