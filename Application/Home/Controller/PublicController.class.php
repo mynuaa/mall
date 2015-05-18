dc4827b2931729ecb5f6c393afc4b6790f10356d
Rex Zeng <rex.fgtsky@qq.com>
2015-05-18 15:07:36 +0800

    全面转换为cookie验证

--- Application/Home/Controller/PublicController.class.php
+++ Application/Home/Controller/PublicController.class.php
@@ -7,14 +7,16 @@ class PublicController extends Controller
 {
     public function _initialize() 
     {
-        print_r($this->autologin());
+        echo $this->autologin();
     }
 
     public function autologin()     //自动登录
     {
-        if (isset($_SESSION['myauth_uid'])) {
-            $_SESSION['uid'] = $_SESSION['myauth_uid'];
-            $_SESSION['username'] = uc_get_user($_SESSION['myauth_uid'], 1)[1];
+        if (isset($_COOKIE['myauth_uid'])) {
+            $uid = explode("\t", uc_authcode($_COOKIE['myauth_uid'], 'DECODE', 'myauth'));
+            $uid = intval($uid[1]);
+            $_SESSION['uid'] = $uid;
+            $_SESSION['username'] = uc_get_user($uid, 1)[1];
         }
         else {
             unset($_SESSION['uid']);
