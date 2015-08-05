8e05917a28d56b3bf8259d74205fb613ed5504ed
Rex Zeng <rex.fgtsky@qq.com>
2015-08-05 11:21:46 +0800

    修复failed的问题

--- Application/Mall/Controller/PublicController.class.php
+++ Application/Mall/Controller/PublicController.class.php
@@ -12,21 +12,30 @@ class PublicController extends Controller
     // 通过本应用的公钥加密
     public function my_encrypt($str) {
         $public_key = openssl_pkey_get_public(PUBLIC_KEY);
-        openssl_public_encrypt($str, $encrypted, $public_key) || die('failed');
+        if (!openssl_public_encrypt($str, $encrypted, $public_key)) return false;
         return $encrypted;
     }
     // 通过本应用的私钥解密
     public function my_decrypt($str) {
         $encrypted = base64_decode($str);
         $private_key = openssl_pkey_get_private(PRIVATE_KEY);
-        openssl_private_decrypt($encrypted, $str, $private_key) || die('failed');
+        if (!openssl_private_decrypt($encrypted, $str, $private_key)) return false;
         return $str;
+    }
+    public function getuid() {
+        if (!($uid = $this->my_decrypt($_COOKIE['myauth_uid']))) {
+            setcookie('myauth_uid', '', time() - 3600);
+            return false;
+        }
+        if (!$uid) return false;
+        $uid = json_decode($uid, true);
+        $uid = intval($uid['uid']);
+        return $uid;
     }
     public function autologin()     //自动登录
     {
         if (isset($_COOKIE['myauth_uid'])) {
-            $uid = json_decode($this->my_decrypt($_COOKIE['myauth_uid']), true);
-            $uid = intval($uid['uid']);
+            $uid = $this->getuid();
             $_SESSION['uid'] = $uid;
             $_SESSION['username'] = uc_get_user($uid, 1)[1];
         }
