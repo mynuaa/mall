c9cf0685a708fe0217501e9b592bb5908f09639e
Rex Zeng <rex.fgtsky@qq.com>
2015-08-10 17:41:47 +0800

    添加微信入口

--- Application/Mall/Controller/PublicController.class.php
+++ Application/Mall/Controller/PublicController.class.php
@@ -7,7 +7,28 @@ class PublicController extends Controller
 {
     public function _initialize() 
     {
+        $this->checkwechat();
         echo $this->autologin();
+    }
+    public function checkwechat() {
+        if (!preg_match('/micromessenger/i', $_SERVER['HTTP_USER_AGENT'])) return;
+        if (isset($_COOKIE['myauth_uid'])) return;
+        if (isset($_GET['code'])) {
+            // 获取openid
+            $result = file_get_contents("https://api.weixin.qq.com/sns/oauth2/access_token?appid=wxa04c7656484a07d2&secret=66fe85f09de7ce2fac6d11e075886686&code={$_GET['code']}&grant_type=authorization_code");
+            $result = json_decode($result, true);
+            $openid = json_encode($result['openid']);
+            // 处理绑定/登录
+            $user = file_get_contents("http://my.nuaa.edu.cn/sso/?action=getuserbyopenid&openid={$openid}");
+            $user = json_decode($user, true);
+            $arr = ['uid' => $uid];
+            setcookie('myauth_uid', $this->my_encrypt(json_encode($arr)), time() + 3600 * 10000, '/', NULL, NULL, true);
+        }
+        else {
+            // 跳到授权页面
+            header('Location: https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxa04c7656484a07d2&redirect_uri=' . rawurlencode("http://{$_SERVER['HTTP_HOST']}{$_SERVER['REQUEST_URI']}") . '&response_type=code&scope=snsapi_base&state=mynuaa#wechat_redirect');
+            exit();
+        }
     }
     // 通过本应用的公钥加密
     public function my_encrypt($str) {
