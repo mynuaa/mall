50ed563f7e55d54ff6ba346f326a1d7c1cec3c29
Rex Zeng <rex.fgtsky@qq.com>
2015-09-06 18:16:44 +0800

    test

--- Application/Home/Controller/PublicController.class.php
+++ Application/Home/Controller/PublicController.class.php
@@ -23,6 +23,7 @@ class PublicController extends Controller
             $user = json_decode($user, true);
             $arr = ['uid' => $uid];
             setcookie('myauth_uid', $this->my_encrypt(json_encode($arr)), time() + 3600 * 10000, '/', NULL, NULL, true);
+            echo json_encode($arr);
         }
         else {
             // 跳到授权页面
