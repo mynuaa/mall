5962806f07bc92dfa18936ce158e0510718abd4f
Rex Zeng <rex.fgtsky@qq.com>
2015-09-06 18:09:05 +0800

    test

--- Application/Home/Controller/PublicController.class.php
+++ Application/Home/Controller/PublicController.class.php
@@ -22,6 +22,8 @@ class PublicController extends Controller
             $user = file_get_contents("http://my.nuaa.edu.cn/sso/?action=getuserbyopenid&openid={$openid}");
             $user = json_decode($user, true);
             $arr = ['uid' => $uid];
+            var_dump($arr);
+            exit();
             setcookie('myauth_uid', $this->my_encrypt(json_encode($arr)), time() + 3600 * 10000, '/', NULL, NULL, true);
         }
         else {
