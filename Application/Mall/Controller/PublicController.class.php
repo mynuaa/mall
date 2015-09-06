1b5dff3d5a9dc0b25aaa0baced407dd1ca1645d6
Rex Zeng <rex.fgtsky@qq.com>
2015-09-06 18:36:59 +0800

    test

--- Application/Mall/Controller/PublicController.class.php
+++ Application/Mall/Controller/PublicController.class.php
@@ -23,6 +23,7 @@ class PublicController extends Controller
             $user = file_get_contents("http://my.nuaa.edu.cn/sso/?action=getuserbyopenid&openid={$openid}");
             $user = json_decode($user, true);
             $arr = ['uid' => $user['uid']];
+            echo "http://my.nuaa.edu.cn/sso/?action=getuserbyopenid&openid={$openid}";
             var_dump($user);
             die();
             setcookie('myauth_uid', $this->my_encrypt(json_encode($arr)), time() + 3600 * 10000, '/', NULL, NULL, true);
