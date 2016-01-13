b18326a2829888649b1388d9c9bb914bf7aed8cb
Rex Zeng <rex.fgtsky@qq.com>
2016-01-13 16:09:58 +0800

    .

--- Application/Mall/Controller/TestController.class.php
+++ Application/Mall/Controller/TestController.class.php
@@ -2,7 +2,7 @@
 namespace Mall\Controller;
 use Think\Controller;
 Vendor('ThinkphpUcenter.UcApi');
-class TestController extends PublicController {
+class TestController extends Controller {
     public function index()
     {
         var_dump(session('?uid'));
